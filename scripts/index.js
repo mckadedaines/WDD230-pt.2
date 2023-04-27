56a6fa538e7ba3d885e011b3c734447d8d91532f
Mckade Daines <mckadedaines1@gmail.com>
2023-04-27 14:21:12 -0700

    fixed current year js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
 let currentdate = document.lastModified;
 document.querySelector("#update").textContent = currentdate;
 console.log(currentdate);
+
+let year = new Date().getFullYear();
+document.querySelector("#currentYear").textContent = year;
+console.log(year);

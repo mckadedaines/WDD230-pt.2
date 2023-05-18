d65f09fdb77a442a944e5a3c5b246533411a495a
Mckade Daines <mckadedaines1@gmail.com>
2023-05-18 14:51:17 -0700

    Dom Mani Finished

---
+++ lesson5/bom.js
@@ -0,0 +1,20 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector(".list");
+
+button.addEventListener("click", function(){
+    if (input.value !== '') {
+        const li = document.createElement("li");
+        const del = document.createElement("button");
+
+        li.innerText = input.value;
+        del.textContent = "❌";
+
+        list.append(li, del);
+        
+        del.addEventListener("click", function(){
+            li.remove();
+            del.remove();
+        });
+    }
+});

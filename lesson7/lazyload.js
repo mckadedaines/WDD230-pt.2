84774b81380bc814bcf957b0184cbcf6b2099192
Mckade Daines <mckadedaines1@gmail.com>
2023-06-01 15:25:55 -0700

    lazy loading done

---
+++ lesson7/lazyload.js
@@ -0,0 +1,65 @@
+const images = document.querySelectorAll("img");
+const options = {threshold: 1, rootMargin: "0px 0px 100px 0px"};
+
+function preloadImage(img) {
+    const source = img.getAttribute('data-src');
+    if(!source) {
+        return;
+    }
+
+    img.src = source;
+}
+
+const io = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            }
+            else {
+                preloadImage(entry.target);
+                io.unobserve(entry.target);
+            }
+        })
+    }, options
+);
+
+images.forEach(image => {
+    io.observe(image);
+})
+
+// local storage
+localStorage.setItem('msg1', 'Hello Permanent');
+sessionStorage.setItem('msg2', 'Hi Temporary');
+console.log(localStorage.getItem('msg1'));
+
+
+// Days since last visit
+// let visit_heading = document.querySelector('.visited');
+
+
+
+// Gets the date
+let now_date = new Date();
+
+// Converts the string in the local storage back to the date. NEED
+// TO DO IT EVERYTIME YOU NEED TO PULL DATA OUT OF THE STORAGE!!!
+
+// This is this weekends homework to calculate the difference in days they visited
+let stored_date = localStorage.getItem('last_visited');
+let last_date = new Date(stored_date); 
+
+if(!stored_date) {
+    visit_heading.textContent = "Welcome!";
+}
+else {
+    // calc the diff to the two dates
+    let diff_time = now_date.getTime() - last_date.getTime();
+
+    let diff_day = Math.round(diff_time/(1000 * 60 * 60 * 24));
+
+    visit_heading.textContent = diff_day + " days since your last visit!";
+}
+
+// Converts the date into a string in the local storage
+localStorage.setItem('last_visited', now_date);

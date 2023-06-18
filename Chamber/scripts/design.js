eba9c52a6e0f7584093992884e13ab6f1346a35c
Mckade Daines <mckadedaines1@gmail.com>
2023-06-17 18:47:40 -0700

    banner added

--- Chamber/scripts/design.js
+++ Chamber/scripts/design.js
@@ -25,17 +25,15 @@ function myFunction() {
   }
 }
 
-
-
 // Handles picture size changing for W06 Responsive Design
-window.addEventListener('DOMContentLoaded', function() {
-  var pictureElement = document.querySelector('picture');
-  var imgElement = pictureElement.querySelector('img');
-  var textOverlayElement = document.querySelector('.text-overlay');
+window.addEventListener("DOMContentLoaded", function () {
+  var pictureElement = document.querySelector("picture");
+  var imgElement = pictureElement.querySelector("img");
+  var textOverlayElement = document.querySelector(".text-overlay");
 
   function updateTextOverlay() {
     var screenWidth = window.innerWidth;
-    var text = '';
+    var text = "";
 
     if (screenWidth > 900) {
       text = "Large Picture";
@@ -48,7 +46,30 @@ window.addEventListener('DOMContentLoaded', function() {
     textOverlayElement.textContent = text;
   }
 
-  window.addEventListener('resize', updateTextOverlay);
+  window.addEventListener("resize", updateTextOverlay);
 
   updateTextOverlay();
 });
+
+// USE FOR TESTING BANNER
+// var testDay = 1;
+var today = new Date().getDay();
+
+
+// USE FOR TESTING BANNER
+// if (testDay === 1 || testDay === 2) {
+if (today === 1 || today === 2) {
+  // Create the banner element
+  var banner = document.createElement("div");
+  banner.className = "banner";
+  banner.innerText =
+    "Come join us for the Chamber meet and greet Monday and Tuesday at 7:00pm!";
+
+  // Append the banner to the body
+  document.body.appendChild(banner);
+
+  // Show the banner
+  banner.style.display = "block";
+
+  document.body.style.paddingTop = "40px";
+}
